ef210d9728e754fee758a1360e1e25c7f44488bd
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-05-23 14:51:29 -0700

    rework Book constructor to take input values as properties, add function to create new Book object on button click

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let myLibrary = [];
 
-// constructor
-function Book (name, author, pageCount, hasRead) {
-  this.name = name
-  this.author = author
-  this.pageCount = pageCount
-  this.hasRead = hasRead
+// Book constructor
+function Book () {
+  this.title = document.querySelector("#title").value
+  this.author = document.querySelector("#author").value
+  this.pageCount = document.querySelector("#pageCount").value
+  this.hasRead = document.querySelector("#hasRead").value
 }
-
-const bookOne = new Book ("Dune", "Frank Herbert", "591", true)
-
 // appends the parameter book to the library[] array
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
-addBookToLibrary(bookOne)
+function createBook() {
+  // add protective logic to prevent submission of items with any undefined fields
+  let libraryBook = new Book()
+  return libraryBook
+}
 
-console.log(myLibrary)
+// creates event listener to create new books and add them to the library array
+
+function createButtonListeners() {
+  const btn = document.querySelector("#btn");
+  btn.onclick = () => {
+    // calls createBook which creates a libraryBook object which is passed to addBookToLibrary, which pushes the object to the myLibrary array
+    addBookToLibrary(createBook())
+    console.log(myLibrary)
+  }
+}
+
+createButtonListeners()
