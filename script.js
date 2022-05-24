5c688ffabde9d4df3b94b8e06e9f7adebb60fd9d
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-05-24 10:24:54 -0700

    add createCard and createCardContent functions

--- script.js
+++ script.js
@@ -11,56 +11,35 @@ function Book () {
 // appends the parameter book to the library[] array
 function addBookToLibrary(book) {
   myLibrary.push(book)
-  createCard(book)
 }
 
-function createBook() {
-  let libraryBook = new Book();
-  return libraryBook;
-};
+const libraryBook = new Book();
+addBookToLibrary(libraryBook)
 
-// creates event listener to create new books and add them to the library array
-
-function createAddButtonListener() {
-  const btn = document.querySelector("#addBook");
-  btn.onclick = () => {
-    // calls createBook which creates a libraryBook object which is passed to addBookToLibrary, which pushes the object to the myLibrary array
-    let libraryBook = createBook()
-    // lines 28-29 check the object created in 26 for empty properties and only pushes it to the myLibrary array if all properties have values
-    for (let key in libraryBook) {
-      if (libraryBook[key] === "") return;
-    };
-    addBookToLibrary(libraryBook)
-    console.log(myLibrary)
-    createRemoveButtonListener()
+function createCard() {
+  // loop to iterate through library array creating a card for each - will be saved as
+  const cardHolder = document.querySelector(".cardHolder")    // selects cardHolder div to use as a parent for divs created by the for loop
+  // lines 23 - 28 create cards and assign them a data-index property which we'll later use to reference them for the delete button
+  for (let i in myLibrary) {
+    let card = document.createElement("div");
+    card.classList.add("card")
+    card.setAttribute("data-index", i)
+    cardHolder.appendChild(card)
+    createCardContent(myLibrary[i])
+    // console.log(myLibrary[i])
   }
 }
 
-// create book card element
-function createCard(libraryBook) {
-  // loops through the items in the libraryBook object and creates a div with textContent = submitted values appended below cardHolder
-
-  const cardHolder = document.querySelector(".cardHolder")
-  const card = document.createElement("div")
-  card.classList.add("card")
-  cardHolder.appendChild(card)
-
-  for (key in libraryBook) {
-    property = document.createElement("div")
-    property.textContent = `${key[0].toUpperCase() + key.slice(1)}: ${libraryBook[key]}`
-    card.appendChild(property)
+// loops through each property in the Book object, creating a div for each containing it's
+function createCardContent(obj) {
+  const newCard = document.querySelector(".card")
+  for (const property in obj) {
+    let cardContent = document.createElement("div");
+    cardContent.textContent = `${property[0].toUpperCase() + property.slice(1)}: ${obj[property]}`
+    cardContent.classList.add("property")
+    newCard.appendChild(cardContent)
+    // console.log(cardContent)
   }
-  // adds remove button to each card
-  const btn = document.createElement("button")
-  btn.textContent = "Remove";
-  btn.setAttribute("id", "removeBook")
-  card.appendChild(btn)
 }
 
-// creates remove button listener
-function createRemoveButtonListener() {
-  const btn = document.querySelector("#removeBook");
-  btn.onclick = () => console.log("hi")
-}
-
-createAddButtonListener()
+createCard()
