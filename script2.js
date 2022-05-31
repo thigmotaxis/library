5bbd3bdcdd020103249f7e6475103f8bbb19a8eb
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-05-31 15:28:17 -0700

    add remaining form inputs, add logic to prevent superfluous LIs

--- script2.js
+++ script2.js
@@ -2,10 +2,11 @@ const container = document.querySelector(".formContainer")
 const btn = document.querySelector(".newBook")
 let i = 0
 btn.onclick = () => {
-  while (i < 3) {                               // prevents more than 3 input/label pairs from being generated
+  while (i < 5) {                               // prevents more than 5 input/label pairs from being generated
   const list = createList()
   const listItem = createListItem(list)
-    createWidgets(listItem)
+    createLabels(listItem)
+    createInputs(listItem)
   i ++
   }
 }
@@ -23,7 +24,7 @@ function createListItem(parent) {
   return listItem
 }
 
-function createWidgets(parent) {
+function createLabels(parent) {
   const label = document.createElement("label")
   if (parent.dataset.index === "0") {
     label.setAttribute("for", "#author")
@@ -40,21 +41,43 @@ function createWidgets(parent) {
     label.textContent = "Page Count: "
     parent.appendChild(label)
   }
+  if (parent.dataset.index === "3") {
+    label.setAttribute("for", "read")
+    label.textContent = "Read: "
+    parent.appendChild(label)
+  }
+}
 
+function createInputs(parent) {
   const input = document.createElement("input")
   if (parent.dataset.index === "0") {
     input.setAttribute("type", "text")
     input.setAttribute("id", "#author")
+    input.setAttribute("required", "")
     parent.appendChild(input)
   }
   if (parent.dataset.index === "1") {
     input.setAttribute("type", "text")
     input.setAttribute("id", "#title")
+    input.setAttribute("required", "")
     parent.appendChild(input)
   }
   if (parent.dataset.index === "2") {
     input.setAttribute("type", "number")
     input.setAttribute("id", "#pageCount")
+    input.setAttribute("required", "")
+    parent.appendChild(input)
+  }
+  if (parent.dataset.index === "3") {
+    input.setAttribute("type", "checkbox")
+    input.setAttribute("id", "read")
+    input.setAttribute("checked", "")
+    parent.appendChild(input)
+  }
+  if (parent.dataset.index === "4") {
+    input.setAttribute("type", "submit")
+    input.setAttribute("id", "submit")
+    input.setAttribute("value", "Submit")
     parent.appendChild(input)
   }
 }
